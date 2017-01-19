ad198d9bac9e97a60738437c23faee4048188cb2
Priyank gupta <priyaaank@gmail.com>
2017-01-19 21:29:07 +0530

    Added children nodes to the tree node

--- src/com/example/domain/TreeNode.java
+++ src/com/example/domain/TreeNode.java
@@ -3,12 +3,31 @@ package com.example.domain;
 public class TreeNode<T> {
 
     private T value;
+    private TreeNode<T> right;
+    private TreeNode<T> left;
 
-    public TreeNode(T value) {
+    public TreeNode(final T value) {
         this.value = value;
     }
 
-    public TreeNode right() {
-        return null;
+    public void setRight(final TreeNode<T> right) {
+        this.right = right;
+    }
+
+    public void setLeft(final TreeNode<T> left) {
+        this.left = left;
+    }
+
+    public TreeNode<T> rightNode() {
+        return right;
+    }
+
+    public TreeNode<T> leftNode() {
+        return this.left;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Value] %s", this.value);
     }
 }
--- test/com/example/domain/TreeNodeTest.java
+++ test/com/example/domain/TreeNodeTest.java
@@ -1,15 +1,44 @@
 package com.example.domain;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
 
 public class TreeNodeTest {
 
-    @Test
-    public void testNodeValuesCanBeSet() {
-        TreeNode<String> rootNode = new TreeNode<>("Root value");
-        assertNull(rootNode.right());
+    private TreeNode<String> rootNode;
+
+    @Before
+    public void setUp() throws Exception {
+        rootNode = new TreeNode<>("Root value");
     }
 
+    @Test
+    public void testNodeStringRepresentationPrintsNodeValue() {
+        assertEquals("[Value] Root value", rootNode.toString());
+    }
+
+    @Test
+    public void testNodeStringRepresentationPrintsNodeValueForNumericValues() {
+        TreeNode<Integer> rootNode = new TreeNode<>(28);
+
+        assertEquals("[Value] 28", rootNode.toString());
+    }
+
+    @Test
+    public void testRightNodeCanBeRetrieved() throws Exception {
+        TreeNode<String> rightNode = new TreeNode<>("Right node");
+        rootNode.setRight(rightNode);
+
+        assertEquals(rightNode, rootNode.rightNode());
+    }
+
+    @Test
+    public void testThatLeftNodeCanBeRetrieved() throws Exception {
+        TreeNode<String> leftNode = new TreeNode<>("Right node");
+        rootNode.setLeft(leftNode);
+
+        assertEquals(leftNode, rootNode.leftNode());
+    }
 }
