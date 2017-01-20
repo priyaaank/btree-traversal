d4ecd52eaabf72b9c61c25fea00e335247771dcb
Priyank gupta <priyaaank@gmail.com>
2017-01-20 12:07:40 +0530

    Adding an integration test for the console app class

--- src/com/example/ConsoleApp.java
+++ src/com/example/ConsoleApp.java
@@ -1,19 +1,23 @@
 package com.example;
 
 import com.example.io.ConsoleOutputManager;
+import com.example.io.OutputManager;
 import com.example.support.Constants;
 import com.example.wizard.CaptureInputStep;
 import com.example.wizard.WizardStep;
 
 public class ConsoleApp implements Constants.IOConstants {
 
-    public static void main(String[] args) {
-        ConsoleOutputManager outputManager = new ConsoleOutputManager();
+    public void execute(OutputManager outputManager) {
         WizardStep wizardStep = new CaptureInputStep(outputManager);
         while (wizardStep != null) {
             wizardStep.execute();
             if (wizardStep.isComplete()) wizardStep = wizardStep.nextStep();
         }
+    }
+
+    public static void main(String[] args) {
+        new ConsoleApp().execute(new ConsoleOutputManager());
     }
 
 }
