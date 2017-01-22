460d16d1c8b881bc1b4e055435210e3d8c08cbb3
Priyank gupta <priyaaank@gmail.com>
2017-01-22 10:28:23 +0530

    Removing uneccessary indirection in traversal strategies

--- src/com/example/domain/InOrderTraverser.java
+++ src/com/example/domain/InOrderTraverser.java
@@ -4,13 +4,9 @@ public class InOrderTraverser<T extends Comparable> implements Traverser<T> {
 
     @Override
     public void traverse(Node<T> node, Visit<Node<T>> visitCallback) {
-        traverseFromNode(node, visitCallback);
-    }
-
-    private void traverseFromNode(Node<T> node, Visit<Node<T>> visitCallback) {
-        if (node.getLeft() != null) traverseFromNode(node.getLeft(), visitCallback);
+        if (node.getLeft() != null) traverse(node.getLeft(), visitCallback);
         visitCallback.visited(node);
-        if (node.getRight() != null) traverseFromNode(node.getRight(), visitCallback);
+        if (node.getRight() != null) traverse(node.getRight(), visitCallback);
     }
 
 }
--- src/com/example/domain/PostOrderTraverser.java
+++ src/com/example/domain/PostOrderTraverser.java
@@ -4,12 +4,8 @@ public class PostOrderTraverser<T extends Comparable> implements Traverser<T> {
 
     @Override
     public void traverse(Node<T> node, Visit<Node<T>> visitCallback) {
-        traverseFromNode(node, visitCallback);
-    }
-
-    private void traverseFromNode(Node<T> node, Visit<Node<T>> visitCallback) {
-        if (node.getLeft() != null) traverseFromNode(node.getLeft(), visitCallback);
-        if (node.getRight() != null) traverseFromNode(node.getRight(), visitCallback);
+        if (node.getLeft() != null) traverse(node.getLeft(), visitCallback);
+        if (node.getRight() != null) traverse(node.getRight(), visitCallback);
         visitCallback.visited(node);
     }
 
